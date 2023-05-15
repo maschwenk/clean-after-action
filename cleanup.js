a168580d81839e1acec6e036ef05257793a5a40e
Max Schwenk <maschwenk@gmail.com>
2023-05-15 16:43:58 +0000

    Run docker cleanup steps after each build

--- cleanup.js
+++ cleanup.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core");
 const io = require("@actions/io");
 const fs = require("fs");
+const { getExecOutput } = require("@actions/exec");
 
 function getBoolValue(name) {
   return ["true", "1", "yes", "y"].includes(
@@ -8,21 +9,32 @@ function getBoolValue(name) {
   );
 }
 
+async function rimRafWorkingDir() {
+  const keepGit = getBoolValue("keep-git");
+  await fs.readdir(".", async (err, files) => {
+    if (err) {
+      throw new Error(`Failed to list files: ${err}`);
+    }
+    for (const file of files) {
+      if (keepGit && file === ".git") {
+        continue;
+      }
+      console.log(`Deleting ${file}`);
+      await io.rmRF(file);
+    }
+  });
+}
+
+async function cleanupDanglingDockerResources() {
+  const runningStuff = await getExecOutput('docker ps -a -q');
+  await getExecOutput(`docker stop ${runningStuff.stdout}`);
+}
+
+
 async function main() {
   try {
-    const keepGit = getBoolValue("keep-git");
-    await fs.readdir(".", async (err, files) => {
-      if (err) {
-        throw new Error(`Failed to list files: ${err}`);
-      }
-      for (const file of files) {
-        if (keepGit && file === ".git") {
-          continue;
-        }
-        console.log(`Deleting ${file}`);
-        await io.rmRF(file);
-      }
-    });
+    await rimRafWorkingDir();
+    await cleanupDanglingDockerResources();
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -31,3 +43,4 @@ async function main() {
 main()
   .then(() => console.log("Finished"))
   .catch((err) => console.log(`Failed to delete files: ${err}`));
+
